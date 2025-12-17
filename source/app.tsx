f88712f3cdd5e10328e3c6d3a151f2d9c179bb42
nakano16180 <nakano1618@gmail.com>
2025-12-17 15:44:51 +0900

    feat: use react hooks

--- source/app.tsx
+++ source/app.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import {Text} from 'ink';
+import React, {useEffect, useState} from 'react';
+import {render, Text} from 'ink';
 
 type Props = {
 	name: string | undefined;
 };
 
 export default function App({name = 'Stranger'}: Props) {
-	return (
-		<Text>
-			Hello, <Text color="green">{name}</Text>
-		</Text>
-	);
+	const [time, setTime] = useState(new Date());
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTime(new Date());
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	return <Text color="green">{time.toLocaleTimeString()}</Text>;
 }
